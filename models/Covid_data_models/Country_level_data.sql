b15532f9376b29320d4d5b50eacb70ce12efd44d
Saurabh <kushandwisdom1074@gmail.com>
2022-03-13 14:57:41 +0530

    Create Country_level_data.sql

---
+++ models/Covid_data_models/Country_level_data.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='table') }} 
+
+with Country_level_data as (
+select date,
+sum(TOTAL_CASES) as TOTAL_CASES,
+sum(TOTAL_ACTIVE_CASES) as TOTAL_ACTIVE_CASES ,
+sum(TOTAL_DEATHS) as TOTAL_DEATHS,
+sum(NEW_CASES) as NEW_CASES,
+sum(NEW_DEATHS) as NEW_DEATHS,
+sum(TOTAL_CASES_PER_MILLION) as TOTAL_CASES_PER_MILLION ,
+sum(NEW_CASES_PER_MILLION) as NEW_CASES_PER_MILLION,
+sum(TOTAL_DEATHS_PER_MILLION) as TOTAL_DEATHS_PER_MILLION
+from FIVETRAN_INTERVIEW_DB.GOOGLE_SHEETS.COVID_19_INDONESIA_Saurabh_sharma
+where LOCATION_LEVEL = 'Country'
+group by 1
+
+)
+
+
+select * from Country_level_data
+
+ 
